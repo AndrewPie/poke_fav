11bbdbd14cc94b4ae205af8381729a7df1e59ec3
AndrewPie <andrzej_pietrzyk@o2.pl>
2021-08-09 02:02:33 +0200

    added possibility to jump back and forward through the list of pokemons

--- pokemon/views.py
+++ pokemon/views.py
@@ -3,14 +3,32 @@ from django.shortcuts import redirect, render
 from django.views.generic import ListView
 from pokemon.models import Pokemon
 
-from django.http import HttpResponse
+# from django.http import HttpResponse, HttpResponseRedirect
+# from django.urls import reverse
 
 class PokemonList(ListView):
     model = Pokemon
     template_name = "pokemon/list.html"
+    
+    def post(self,request):
+        if 'first' in request.POST:
+            self.request.session['offset'] = 0
+        elif 'next' in request.POST:
+            self.request.session['offset'] += 10
+        elif 'previous' in request.POST:
+            self.request.session['offset'] -= 10
+        elif 'last' in request.POST:
+            self.request.session['offset'] = 1110
+# TODO: "last" must be dynamically changed depending on api "count" value
+        # return HttpResponseRedirect(reverse('pokemon:list'))
+        # return HttpResponseRedirect('')
+        return redirect('pokemon:list')
+
 
     def call_pokemon_api(self):
-        payload = {'offset': 0, 'limit': 10}
+        payload = {
+            'offset': self.request.session['offset'], 'limit': 10
+            }
         response = requests.get('https://pokeapi.co/api/v2/pokemon/', params = payload)
         data = response.json()
         return data
@@ -19,7 +37,7 @@ class PokemonList(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         if not 'offset' in self.request.session or not self.request.session['offset']:
-            self.request.session['offset'] = 10
+            self.request.session['offset'] = 0
         context['pokemon'] = self.call_pokemon_api
         context['offset'] = self.request.session['offset']
         return context
