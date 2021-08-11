ee7d25281cfc93490d3e097858dac0a2a3b1f610
AndrewPie <andrzej_pietrzyk@o2.pl>
2021-08-12 00:50:56 +0200

    Last page of paginated ListView is now dependent on 'count' value reveived from API. Moved call_poke_api() outside class view and now it is called from overwritten setup method inside PokemList(ListView).

--- pokemon/views.py
+++ pokemon/views.py
@@ -1,17 +1,41 @@
 import requests
+import json
 from django.shortcuts import redirect, render
 from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from pokemon.models import Pokemon
 
 # from django.http import HttpResponse, HttpResponseRedirect
-# from django.urls import reverse
+
+
+def call_poke_api(self, request, *args, **kwargs):
+    if not 'offset' in self.request.session or not self.request.session['offset']:
+            self.request.session['offset'] = 0
+
+    payload = {
+            'offset': self.request.session['offset'], 
+            'limit': 10
+            }
+    response = requests.get('https://pokeapi.co/api/v2/pokemon/', params = payload)
+    data = response.json()
+    return data
+
 
 class PokemonList(LoginRequiredMixin, ListView):
     model = Pokemon
     template_name = "pokemon/list.html"
     
-    def post(self,request):
+    def setup(self, request, *args, **kwargs):
+        if hasattr(self, 'get') and not hasattr(self, 'head'):
+            self.head = self.get
+        self.request = request
+        self.args = args
+        self.kwargs = kwargs
+        
+        self.data = call_poke_api(self, request)
+        super().setup
+
+    def post(self, request):
         if 'first' in request.POST:
             self.request.session['offset'] = 0
         elif 'next' in request.POST:
@@ -19,26 +43,14 @@ class PokemonList(LoginRequiredMixin, ListView):
         elif 'previous' in request.POST:
             self.request.session['offset'] -= 10
         elif 'last' in request.POST:
-            self.request.session['offset'] = 1110
-# TODO: "last" must be dynamically changed depending on api "count" value
+            last_page = (self.data['count']//10)*10
+            self.request.session['offset'] = last_page
         # return HttpResponseRedirect(reverse('pokemon:list'))
         # return HttpResponseRedirect('')
         return redirect('pokemon:list')
 
-
-    def call_pokemon_api(self):
-        payload = {
-            'offset': self.request.session['offset'], 'limit': 10
-            }
-        response = requests.get('https://pokeapi.co/api/v2/pokemon/', params = payload)
-        data = response.json()
-        return data
-
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        if not 'offset' in self.request.session or not self.request.session['offset']:
-            self.request.session['offset'] = 0
-        context['pokemon'] = self.call_pokemon_api
-        context['offset'] = self.request.session['offset']
+        context['pokemon'] = self.data
+        # print(self.request.session['offset'])
         return context
